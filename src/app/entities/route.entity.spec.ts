96f2006a2a7b333621cda6c85fd28585e2a2dbd4
Lucas Oliveira Rosendo <lucas.rosendo@rotatransportes.com.br>
2023-09-22 12:06:21 -0300

    initial config

---
+++ src/app/entities/route.entity.spec.ts
@@ -0,0 +1,5 @@
+describe('Route tests', () => {
+  test('constructor', () => {
+    
+  })
+})
---
+++ src/app/entities/route.entity.ts
@@ -0,0 +1,23 @@
+export type LatLng = { lat: number; lng: number };
+
+export type RouteProps = {
+  title: string;
+  startPosition: LatLng;
+  endPosition: LatLng;
+  points?: LatLng[];
+}
+
+export class Route {
+  public props: Required<RouteProps>;
+  constructor(props: RouteProps){
+    this.props = {
+      ...props,
+      points: props.points || [],
+    };
+  }
+
+  private set title(value: string) {
+    this.props.title = value;
+  }
+  
+}
