c74a1278e2a22eb4764f8441d2746787c33798f4
Lucas Oliveira Rosendo <lucas.rosendo@rotatransportes.com.br>
2023-09-26 07:40:50 -0300

    tests: route entity test

--- src/app/entities/route.entity.spec.ts
+++ src/app/entities/route.entity.spec.ts
@@ -1,5 +1,30 @@
+import { Route, RouteProps } from "./route.entity"
+
 describe('Route tests', () => {
   test('constructor', () => {
-    
+    let routeProps: RouteProps = {
+      title: 'minha rota',
+      startPosition: {lat: 0, lng: 1},
+      endPosition: {lat: 2, lng: 3}
+    }
+    let route = new Route(routeProps)
+    expect(route.props).toStrictEqual({
+      ...routeProps, points: []
+    })
+
+    routeProps = {
+      title: 'minha rota',
+      startPosition: {lat: 0, lng: 1},
+      endPosition: {lat: 2, lng: 3},
+      points: [
+        {lat: 10, lng: 11}
+      ]
+    }
+    route = new Route(routeProps)
+    expect(route.props).toStrictEqual({
+      ...routeProps, points: [
+        {lat: 10, lng: 11}
+      ]
+    })
   })
 })
